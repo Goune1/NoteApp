2d52d093c52ebf13d3d78e5539dbe850d28ac9f9
Goune <71518163+Goune1@users.noreply.github.com>
2024-06-09 14:37:28 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(cors());
 const loginCollection = require("./schemas/loginSchema");
 const notesCollection = require('./schemas/noteSchema');
 
-const DB_URL = "mongodb+srv://goune:goune1407@ac-nef3pac.mongodb.net/NotesApp?retryWrites=true&w=majority";
+const DB_URL = "mongodb://goune:goune1407@ac-nef3pac-shard-00-02.0x9jwgi.mongodb.net:27017,ac-nef3pac-shard-00-01.0x9jwgi.mongodb.net:27017,ac-nef3pac-shard-00-00.0x9jwgi.mongodb.net:27017/WineApp?authSource=admin&replicaSet=atlas-tibrt3-shard-0&ssl=true";
 
 async function connectToDatabase() {
     try {
