e618fe79f22676723314ac3a29619ad0f6ac8ecf
Goune <71518163+Goune1@users.noreply.github.com>
2024-06-16 13:47:14 +0200

    Update layout.tsx

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -16,7 +16,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
-      <meta name="google-site-verification" content="v_WbGzz1R9V-ybvkeHIPB9J8dQWbmgd-hjBNjLHmzrU" />
       <link rel="icon" href="logo.png" />
       <body className={inter.className}>{children}</body>
     </html>
